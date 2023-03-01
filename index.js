a07c06e6b6cc01c27da2a0c61cb109424cf97870
reymendoza94 <rey.mendoza941230@gmail.com>
2023-03-01 11:16:09 -0500

    Build index in graphqlsever

---
+++ index.js
@@ -0,0 +1,21 @@
+const books = [
+    {
+        title: "Django3",
+        author: "",
+        ISBN: "",
+        description: "",
+        front_page: "",
+
+    }
+]
+
+const author = [
+    {
+        name: "",
+        lastname: "",
+        description: "",
+        birthdate: "",
+        photo: "",
+    }
+    
+]
