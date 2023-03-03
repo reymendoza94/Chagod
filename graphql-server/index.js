715a80a1a319fc42a4a9c66aa3c659d86b9420fe
reymendoza94 <reymendoza94>
2023-03-02 23:12:23 -0500

    Create Migration

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -1,9 +1,50 @@
 import { ApolloServer, gql } from "apollo-server"
+import {v1 as uuid} from 'uuid'
 const books = [
     {
         id: '1',
-        titulo: 'A Song of Ice and Fire',
-        author: 'George R. R. Martin',
+        titulo: 'Game of Thrones',
+        author: '',
+        ISBN: '',
+        description: '',
+        fron_page: '',
+        year: ''
+    },
+
+    {
+        id: '2',
+        titulo: 'Clash of Kings',
+        author: '',        
+        ISBN: '',
+        description: '',
+        fron_page: '',
+        year: ''
+    },
+
+    {
+        id: '3',
+        titulo: 'Dance of Dragons',
+        author: '',
+        ISBN: '',
+        description: '',
+        fron_page: '',
+        year: ''
+    },
+
+    {
+        id: '4',
+        titulo: 'Harry Potter and the Philosopher Stone',
+        author: '',
+        ISBN: '',
+        description: '',
+        fron_page: '',
+        year: ''
+    },
+
+    {
+        id: '5',
+        titulo: 'Harry Potter and the Deathly Hallows',
+        author: '',
         ISBN: '',
         description: '',
         fron_page: '',
@@ -16,6 +57,15 @@ const authors = [
         id: '1',
         name:'George R',
         last_name:'R. Martin',
+        extract: '',
+        bithday: '',
+        photo: ''
+    },
+
+    {
+        id: '2',
+        name:'J. K.',
+        last_name:'Rowling',
         extract: '',
         bithday: '',
         photo: ''
@@ -36,17 +86,37 @@ const typeDefs = gql`
         authorCount: Int!
         allAuthors: [Author]!
     }
+
+    type Mutation {
+        addAuthor(
+            name: String!
+            last_name: String!
+            extract: String!
+            bithday: String!
+            photo: String
+        ): Author
+    }
 `
 const resolvers = {
     Query: {
         authorCount: () => authors.length,
         allAuthors: () => authors
-    }
+    },
+
+    Mutation: {
+        addAuthor: (root, args) => {
+            const author = { ...args, id: uuid()}
+            author = authors.push(author)
+            return author
+        }
+    },
 }
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers   
+    resolvers,
+    introspection: true,
+    playground: true,   
 })
 
 server.listen().then(({url}) => {
